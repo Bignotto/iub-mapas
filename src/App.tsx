7d9812642b58f00a070d17b099d8d2d33f885e2a
Thiago Bignotto <bignotto@gmail.com>
2020-10-29 06:51:10 -0300

    added leaflet and react-leaflet

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 const App: React.FC = () => {
-  return <h1>Big Rules!!</h1>;
+  return (
+    <div style={{ flex: 1, minHeight: 600, minWidth: 800 }}>
+      <Map center={[-22.4036192, -47.5963294]} zoom={15}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </Map>
+    </div>
+  );
 };
 
 export default App;
