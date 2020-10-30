6b94a0d142f3eafa6139f4d78a7f8a803bb3d50d
Thiago Bignotto <bignotto@gmail.com>
2020-10-30 06:43:17 -0300

    created auth hook

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,101 @@
+import React, { createContext, useCallback, useState, useContext } from 'react';
+// import api from "../services/api";
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatarUrl: string;
+}
+
+interface AuthState {
+  token: string;
+  user: User;
+}
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
+
+interface AuthContextData {
+  user: User;
+  signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
+  updateUser(user: User): void;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@GoBarber:token');
+    const user = localStorage.getItem('@GoBarber:user');
+
+    if (token && user) {
+      // api.defaults.headers.authorization = `Bearer ${token}`;
+
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as AuthState;
+  });
+
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@GoBarber:token');
+    localStorage.removeItem('@GoBarber:user');
+
+    setData({} as AuthState);
+  }, []);
+
+  const signIn = useCallback(async ({ email, password }) => {
+    // const response = await api.post('session', { email, password });
+
+    if (email !== 'bignotto' && password !== 'melhorcaixao')
+      throw new Error('Usuário e Senha inválidos!');
+
+    const token = 'Bearer: token válido';
+    const user = {
+      id: 'string',
+      name: 'bignotto',
+      email: 'dunha@bignotto.com.br',
+      avatarUrl: 'sem avatar',
+    };
+
+    localStorage.setItem('@GoBarber:token', token);
+    localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    // api.defaults.headers.authorization = `Bearer ${token}`;
+
+    setData({ token, user });
+  }, []);
+
+  const updateUser = useCallback(
+    (user: User) => {
+      localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+      setData({
+        token: data.token,
+        user,
+      });
+    },
+    [setData, data.token],
+  );
+
+  return (
+    <AuthContext.Provider
+      value={{ user: data.user, signIn, signOut, updateUser }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider tag');
+  }
+  return context;
+}
+
+export { AuthProvider, useAuth };
